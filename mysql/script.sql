eaa44f6a987aa5e3690717b7c28eecdf6ca8780f
Ícaro Rodrigo Ferreira Da Fonseca Bezerra <icaro.bezerra@sptech.school>
2024-03-31 17:19:38 -0300

    script mais novo

--- mysql/script.sql
+++ mysql/script.sql
@@ -2,220 +2,160 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema planejamente
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `planejamente` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `planejamente`;
 
--- -----------------------------------------------------
--- Schema Planejamente
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema Planejamente
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Planejamente` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
-USE `Planejamente` ;
-
--- -----------------------------------------------------
--- Table `Planejamente`.`Endereço`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Endereço` (
+-- Table psicologo
+CREATE TABLE IF NOT EXISTS `psicologo` (
   `id` CHAR(36) NOT NULL,
-  `Número` INT NOT NULL,
-  `CEP` CHAR(8) NOT NULL,
-  `Rua` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  `crp` VARCHAR(45) NULL,
+  `cnpj` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Usuário`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Usuário` (
+-- Table endereco
+CREATE TABLE IF NOT EXISTS `endereco` (
   `id` CHAR(36) NOT NULL,
-  `Nome` VARCHAR(255) NOT NULL,
-  `Email` VARCHAR(255) NOT NULL,
-  `Sal` VARCHAR(255) NOT NULL,
-  `Senha` VARCHAR(255) NOT NULL,
-  `DataDeNascimento` DATE NOT NULL,
-  `Telefone` VARCHAR(255) NOT NULL,
-  `Endereço` CHAR(36) NOT NULL,
-  `CPF` VARCHAR(45) NULL,
+  `numero` INT NOT NULL,
+  `cep` CHAR(8) NOT NULL,
+  `rua` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- Table usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` CHAR(36) NOT NULL,
+  `nome` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `sal` VARCHAR(255) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `data_de_nascimento` DATE NOT NULL,
+  `telefone` VARCHAR(255) NOT NULL,
+  `endereco_id` CHAR(36) NOT NULL,
+  `cpf` VARCHAR(45) NOT NULL,
+  `psicologo_id` CHAR(36) NULL,
   PRIMARY KEY (`id`),
-  INDEX `Endereço` (`Endereço` ASC) VISIBLE,
-  CONSTRAINT `Paciente_ibfk_10`
-    FOREIGN KEY (`Endereço`)
-    REFERENCES `Planejamente`.`Endereço` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `Planejamente`.`Psicólogo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Psicólogo` (
-  `id` CHAR(36) NOT NULL,
-  `Usuário_id` CHAR(36) NOT NULL,
-  `CRP` VARCHAR(45) NULL,
-  `CNPJ` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`, `Usuário_id`),
-  INDEX `fk_Psicólogo_Usuário1_idx` (`Usuário_id` ASC) VISIBLE,
-  CONSTRAINT `fk_Psicólogo_Usuário1`
-    FOREIGN KEY (`Usuário_id`)
-    REFERENCES `Planejamente`.`Usuário` (`id`)
+  INDEX `endereco` (`endereco_id` ASC) VISIBLE,
+  INDEX `fk_usuario_psicologo1_idx` (`psicologo_id` ASC) VISIBLE,
+  CONSTRAINT `paciente_ibfk_10`
+    FOREIGN KEY (`endereco_id`)
+    REFERENCES `endereco` (`id`),
+  CONSTRAINT `fk_usuario_psicologo1`
+    FOREIGN KEY (`psicologo_id`)
+    REFERENCES `psicologo` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Consulta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Consulta` (
+-- Table consulta
+CREATE TABLE IF NOT EXISTS `consulta` (
   `id` CHAR(36) NOT NULL,
-  `DataHoraInicio` DATETIME NOT NULL,
-  `DataHoraFim` DATETIME NOT NULL,
-  `Status` VARCHAR(255) NOT NULL,
-  `Observações` VARCHAR(255) NOT NULL,
-  `Psicólogo` CHAR(36) NOT NULL,
-  `Link` VARCHAR(255) NOT NULL,
-  `Usuário_id` CHAR(36) NOT NULL,
+  `data_hora_inicio` DATETIME NOT NULL,
+  `data_hora_fim` DATETIME NOT NULL,
+  `status` VARCHAR(255) NOT NULL,
+  `observacoes` VARCHAR(255) NOT NULL,
+  `psicologo_id` CHAR(36) NOT NULL,
+  `link` VARCHAR(255) NOT NULL,
+  `usuario_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `Psicólogo` (`Psicólogo` ASC) VISIBLE,
-  INDEX `fk_Consulta_Usuário1_idx` (`Usuário_id` ASC) VISIBLE,
-  CONSTRAINT `Consulta_ibfk_2`
-    FOREIGN KEY (`Psicólogo`)
-    REFERENCES `Planejamente`.`Psicólogo` (`id`),
-  CONSTRAINT `fk_Consulta_Usuário1`
-    FOREIGN KEY (`Usuário_id`)
-    REFERENCES `Planejamente`.`Usuário` (`id`)
+  INDEX `psicologo` (`psicologo_id` ASC) VISIBLE,
+  INDEX `fk_consulta_usuario1_idx` (`usuario_id` ASC) VISIBLE,
+  CONSTRAINT `consulta_ibfk_2`
+    FOREIGN KEY (`psicologo_id`)
+    REFERENCES `psicologo` (`id`),
+  CONSTRAINT `fk_consulta_usuario1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `usuario` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Arquivos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Arquivos` (
+-- Table arquivos
+CREATE TABLE IF NOT EXISTS `arquivos` (
   `id` CHAR(36) NOT NULL,
-  `Url` VARCHAR(255) NOT NULL,
-  `Nome` VARCHAR(255) NOT NULL,
-  `Tamanho` INT NOT NULL,
-  `Extensão` VARCHAR(255) NOT NULL,
-  `Consulta` CHAR(36) NOT NULL,
+  `url` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(255) NOT NULL,
+  `tamanho` INT NOT NULL,
+  `extensao` VARCHAR(255) NOT NULL,
+  `consulta_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `Consulta` (`Consulta` ASC) VISIBLE,
-  CONSTRAINT `Arquivos_ibfk_1`
-    FOREIGN KEY (`Consulta`)
-    REFERENCES `Planejamente`.`Consulta` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  INDEX `consulta` (`consulta_id` ASC) VISIBLE,
+  CONSTRAINT `arquivos_ibfk_1`
+    FOREIGN KEY (`consulta_id`)
+    REFERENCES `consulta` (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Avaliação`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Avaliação` (
+-- Table avaliacao
+CREATE TABLE IF NOT EXISTS `avaliacao` (
   `id` CHAR(36) NOT NULL,
-  `Consulta` CHAR(36) NOT NULL,
-  `Paciente` CHAR(36) NOT NULL,
-  `Descrição` VARCHAR(255) NOT NULL,
-  `Usuário_id` CHAR(36) NOT NULL,
-  PRIMARY KEY (`id`, `Usuário_id`),
-  INDEX `Consulta` (`Consulta` ASC) VISIBLE,
-  INDEX `fk_Avaliação_Usuário1_idx` (`Usuário_id` ASC) VISIBLE,
-  CONSTRAINT `Avaliação_ibfk_1`
-    FOREIGN KEY (`Consulta`)
-    REFERENCES `Planejamente`.`Consulta` (`id`),
-  CONSTRAINT `fk_Avaliação_Usuário1`
-    FOREIGN KEY (`Usuário_id`)
-    REFERENCES `Planejamente`.`Usuário` (`id`)
+  `consulta_id` CHAR(36) NOT NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  `usuario_id` CHAR(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `consulta` (`consulta_id` ASC) VISIBLE,
+  INDEX `fk_avaliacao_usuario1_idx` (`usuario_id` ASC) VISIBLE,
+  CONSTRAINT `avaliacao_ibfk_1`
+    FOREIGN KEY (`consulta_id`)
+    REFERENCES `consulta` (`id`),
+  CONSTRAINT `fk_avaliacao_usuario1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `usuario` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Especialidade`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Especialidade` (
+-- Table especialidade
+CREATE TABLE IF NOT EXISTS `especialidade` (
   `id` CHAR(36) NOT NULL,
-  `Descrição` VARCHAR(255) NOT NULL,
-  `Título` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  `descricao` VARCHAR(255) NOT NULL,
+  `titulo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`EspecialidadePsicólogo`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`EspecialidadePsicólogo` (
+-- Table especialidade_psicologo
+CREATE TABLE IF NOT EXISTS `especialidade_psicologo` (
   `id` CHAR(36) NOT NULL,
-  `Especialidade` CHAR(36) NOT NULL,
-  `Psicólogo` CHAR(36) NOT NULL,
+  `especialidade_id` CHAR(36) NOT NULL,
+  `psicologo_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `Especialidade` (`Especialidade` ASC) VISIBLE,
-  INDEX `Psicólogo` (`Psicólogo` ASC) VISIBLE,
-  CONSTRAINT `EspecialidadePsicólogo_ibfk_1`
-    FOREIGN KEY (`Especialidade`)
-    REFERENCES `Planejamente`.`Especialidade` (`id`),
-  CONSTRAINT `EspecialidadePsicólogo_ibfk_2`
-    FOREIGN KEY (`Psicólogo`)
-    REFERENCES `Planejamente`.`Psicólogo` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  INDEX `especialidade` (`especialidade_id` ASC) VISIBLE,
+  INDEX `psicologo` (`psicologo_id` ASC) VISIBLE,
+  CONSTRAINT `especialidadepsicologo_ibfk_1`
+    FOREIGN KEY (`especialidade_id`)
+    REFERENCES `especialidade` (`id`),
+  CONSTRAINT `especialidadepsicologo_ibfk_2`
+    FOREIGN KEY (`psicologo_id`)
+    REFERENCES `psicologo` (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
--- -----------------------------------------------------
--- Table `Planejamente`.`Experiência`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Experiência` (
+-- Table experiencia_formacao
+CREATE TABLE IF NOT EXISTS `experiencia_formacao` (
   `id` CHAR(36) NOT NULL,
-  `DataInicio` DATE NOT NULL,
-  `DataFim` DATE NULL DEFAULT NULL,
-  `Instituição` VARCHAR(255) NOT NULL,
-  `Cargo` VARCHAR(255) NOT NULL,
-  `Descrição` VARCHAR(255) NOT NULL,
-  `Psicólogo` CHAR(36) NOT NULL,
+  `datainicio` DATE NOT NULL,
+  `datafim` DATE NULL DEFAULT NULL,
+  `instituicao` VARCHAR(255) NOT NULL,
+  `cargo` VARCHAR(255) NULL,
+  `descricao` VARCHAR(255) NOT NULL,
+  `psicologo_id` CHAR(36) NOT NULL,
+  `tipo` VARCHAR(45) NULL,
+  `titulo` VARCHAR(45) NULL,
   PRIMARY KEY (`id`),
-  INDEX `Psicólogo` (`Psicólogo` ASC) VISIBLE,
-  CONSTRAINT `Experiência_ibfk_1`
-    FOREIGN KEY (`Psicólogo`)
-    REFERENCES `Planejamente`.`Psicólogo` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `Planejamente`.`Formação`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Planejamente`.`Formação` (
-  `id` CHAR(36) NOT NULL,
-  `Instituição` VARCHAR(255) NOT NULL,
-  `AnoConclusão` VARCHAR(255) NOT NULL,
-  `Tipo` VARCHAR(255) NOT NULL,
-  `Título` VARCHAR(255) NOT NULL,
-  `Psicólogo` CHAR(36) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `Psicólogo` (`Psicólogo` ASC) VISIBLE,
-  CONSTRAINT `Formação_ibfk_1`
-    FOREIGN KEY (`Psicólogo`)
-    REFERENCES `Planejamente`.`Psicólogo` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
+  INDEX `psicologo` (`psicologo_id` ASC) VISIBLE,
+  CONSTRAINT `experiencia_ibfk_1`
+    FOREIGN KEY (`psicologo_id`)
+    REFERENCES `psicologo` (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
